eb418476ca947e88e7989dcd974603ed48b9fbd5
Sarad Mishra <62197447+sharad28@users.noreply.github.com>
2021-09-04 02:01:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 
-import pickle
-from pandas_profiling import ProfileReport
 
 from flask_cors import CORS,cross_origin
 
 import pandas as pd
 
-from sklearn.linear_model import Ridge,Lasso, RidgeCV,LassoCV, ElasticNet, ElasticNetCV, LinearRegression
+from sklearn.linear_model import  ElasticNet, ElasticNetCV, LinearRegression
 from sklearn.model_selection import train_test_split
 
 import lg
@@ -19,7 +17,15 @@ from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
-
+"""
+    # Following are the Data analysis 
+    1. As per data set. air temperature (y) is highly correlated with process temperature. model will take advantage of it.
+    2. As rotational speed is highly correlated with torque. we can drop rotational speed. Additionally rotational speed was a right skwed data set
+    3. random failures (RNF) is not related with target y value. therefore, we will drop it.
+    4. Machine failure is related with many feature. So we will drop it due to multi-collinearity
+    5. As per Phik correlation coefficient torque and PWF are highly related. So we drop PWF
+    6. Try to generate gussian like data for tool wear using sklearn.preprocessing.PowerTransformer
+    7. Torque, Tool wear, TWF, HDF & OSF have null or negligible relation with dependent variable"""
 
 
 
@@ -30,15 +36,16 @@ app = Flask(__name__)  # initialising the flask app with the name 'app'
 @cross_origin()
 def homePage():
     ln = ln_reg()
+    ln.dropfeatures()
+    ln.modeling()
 
-    data = {"ln":100,"ridge":200}
-    return render_template("index.html", content=data)
+    return render_template("index.html")
 
 
 @app.route('/initaldata', methods=['GET', 'POST'])
 def ini():
-    data = 1212
-    return render_template('initaldata.html', content=data)
+
+    return render_template('initaldata.html')
 
 @app.route('/After EDA', methods=['GET', 'POST'])
 def Final():
@@ -51,44 +58,20 @@ class ln_reg:
         try:
             lggg.info('Reading CSV file')
             self.df = pd.read_csv("ai4i2020.csv")
-            self.initialreport()
+
         except Exception as e:
             lggg.excpt(f'exception had occured during file read operation. i.e., {e}')
 
-    def initialreport(self):
-        try:
-            lggg.info('Initial report is preparing')
-            self.pf = ProfileReport(self.df)
-            self.pf.to_file('initaldata.html')
-            self.dropfeatures()
-        except Exception as e:
-            lggg.excpt(f'Exception {e} had occured during report generation')
 
-    """# Following are the Data analysis 
-    1. As per data set. air temperature (y) is highly correlated with process temperature. model will take advantage of it.
-    2. As rotational speed is highly correlated with torque. we can drop rotational speed. Additionally rotational speed was a right skwed data set
-    3. random failures (RNF) is not related with target y value. therefore, we will drop it.
-    4. Machine failure is related with many feature. So we will drop it due to multi-collinearity
-    5. As per Phik correlation coefficient torque and PWF are highly related. So we drop PWF
-    6. Try to generate gussian like data for tool wear using sklearn.preprocessing.PowerTransformer
-    7. Torque, Tool wear, TWF, HDF & OSF have null or negligible relation with dependent variable"""
 
     def dropfeatures(self):
         try:
             lggg.info('Droping unnecceasry data')
             self.df1 = self.df.drop(axis=1,columns=['Rotational speed [rpm]','Machine failure','RNF',
                                           'UDI','Product ID','Type','PWF','Torque [Nm]','Tool wear [min]','TWF','HDF','OSF'])
-            self.finalreport()
+#            self.finalreport()
         except Exception as e:
             lggg.excpt(f'Exception {e} had occured during droping features')
-
-    def finalreport(self):
-        try:
-            lggg.info("Final report is preparing")
-            ProfileReport(self.df1).to_file('After EDA.html')
-            self.modeling()
-        except Exception as e:
-            lggg.excpt(f'Exception {e} had occured during final report preparation')
 
     def modeling(self):
         try:
@@ -102,7 +85,7 @@ class ln_reg:
             self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x, self.y, test_size=0.25)
             self.linear.fit(self.x_train,self.y_train)
             self.file = 'linear_reg.sav'
-            pickle.dump(self.linear,open(self.file,'wb'))
+            #pickle.dump(self.linear,open(self.file,'wb'))
 
             ln = self.linear.score(self.x_test,self.y_test)
             lggg.info(f"Score of linear regression model is {ln}")
@@ -117,6 +100,9 @@ class ln_reg:
 
         except  Exception as e:
             lggg.excpt(f"Exception (i.e.,{e} is occur during modeling")
+
+
+
 @app.route('/Score', methods=['GET', 'POST'])
 def score():
     data= {"linear regression Score is": ln,
@@ -127,5 +113,5 @@ def score():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
